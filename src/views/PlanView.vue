10f3993c3e554b916ab7f0949092aba89c460704
dalsu0222 <dalsu0222@sju.ac.kr>
2025-05-16 14:20:38 +0900

    feat: google Maps api 적용 및 커스텀 훅 정의 (#5)

--- src/views/PlanView.vue
+++ src/views/PlanView.vue
@@ -1,5 +1,16 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { usePlanMap } from '@/composables/plan/usePlanMap';
+
+const { initMap } = usePlanMap();
+
+onMounted(() => {
+  initMap();
+});
+</script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center bg-gray-50"></div>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <div id="map" style="width: 100%; height: 500px"></div>
+  </div>
 </template>
