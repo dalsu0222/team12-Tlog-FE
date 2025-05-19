5926c7910df2c92c67863e66ac8828fe268b77e6
dalsu0222 <dalsu0222@sju.ac.kr>
2025-05-19 16:18:34 +0900

    feat: 공통 레이아웃 적용 (#2)

--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="flex min-h-screen items-center justify-center bg-gray-50"></div>
+  <div class="mx-auto max-w-screen-xl bg-gray-100 px-4 py-4">
+    <h1>회원가입 페이지</h1>
+  </div>
 </template>
